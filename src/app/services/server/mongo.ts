c6abf2b29ce60df3d98cfed5bdb647ebf8bf2ae8
estyrabin <estyrabi@gmail.com>
2025-11-06 11:35:03 +0200

    mongo conection

---
+++ src/app/services/server/mongo.ts
@@ -0,0 +1,10 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri);
+const clientPromise = client.connect();
+
+export async function getDb(dbName = "groupay_db") {
+  const c = await clientPromise;
+  return c.db(dbName);
+}
