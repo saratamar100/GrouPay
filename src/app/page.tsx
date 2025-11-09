d6025289afcd86d0c1caabf1687680073ddc2d51
ayeletfridman <ayfridma@g.jct.ac.il>
2025-11-09 15:12:55 +0200

    build landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import { getDb } from "./services/server/mongo";
+import { LandingPage } from "@/app/components/Home/LandingPage";
 
 export default async function Home() {
   const db = await getDb();
   const result = await db.command({ ping: 1 });
-  console.log("Pinged your deployment. You successfully connected to MongoDB!", result);
-
-  return (
-    <>
-      <h1>Hello world</h1>
-    </>
+  console.log(
+    "Pinged your deployment. You successfully connected to MongoDB!",
+    result
   );
+
+  return <LandingPage />;
 }
