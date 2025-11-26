cceb184d2445b92146275721a2584503129fcf7d
ayeletfridman <ayfridma@g.jct.ac.il>
2025-11-26 12:28:00 +0200

    organize2

--- src/app/groups/new_group/page.tsx
+++ src/app/groups/new_group/page.tsx
@@ -1,15 +1,12 @@
-import { getAllUsersFromDB } from "../../services/server/userService"; 
 import { CreateGroupForm } from "../../components/Groups/CreateGroupForm";
 import Header from "../../components/Header/Header";
 
 export default async function CreateGroupPage() {
-    const allUsers = await getAllUsersFromDB(); 
-
   return (
     <div>
       <Header />
       <main>
-        <CreateGroupForm allUsers={allUsers} />
+        <CreateGroupForm />
       </main>
     </div>
   );
