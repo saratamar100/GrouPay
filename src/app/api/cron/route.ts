58397e0aec2643956ce7f2322ec3ef059cf4768b
שרה תמר אמיתי <saratama@g.jct.ac.il>
2025-12-01 10:09:18 +0200

    auto email

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,25 +1,33 @@
 import { NextResponse } from 'next/server';
-import sgMail from '@sendgrid/mail';
+import nodemailer from 'nodemailer';
 
 export async function GET(req: Request) {
-  //const auth = req.headers.get('Authorization');
-  // if (auth !== `Bearer ${process.env.CRON_SECRET}`) {
-  //   return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
-  // }
+  const url = new URL(req.url);
+  const secret = url.searchParams.get('secret');
 
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
-  console.log("Sending cron email...");
+  if (secret !== process.env.CRON_SECRET) {
+    return new Response(JSON.stringify({ error: 'Unauthorized' }), { status: 401 });
+  }
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.MAIL_USER, 
+      pass: process.env.MAIL_PASS, 
+    },
+  });
 
   try {
-    await sgMail.send({
-      to: process.env.TO_EMAIL!,      
-      from: process.env.FROM_EMAIL!,   
-      subject: 'Hi from Cron Job!',
-      text: "final text 11:30",
+    await transporter.sendMail({
+      from: process.env.MAIL_USER,
+      to: process.env.TO_EMAIL, 
+      subject: "Hi from GroPay",
+      text: "10:15",
     });
 
-    return NextResponse.json({ ok: true, message: 'Email sent!' });
+    return NextResponse.json({ ok: true, message: "Email sent!" });
   } catch (error: any) {
+    console.error(error);
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
