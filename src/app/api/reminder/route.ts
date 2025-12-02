8938e609bd3e9cbac4bae6f2190e8dfec1c966f2
שרה תמר אמיתי <saratama@g.jct.ac.il>
2025-12-02 10:30:14 +0200

    final code not working

--- src/app/api/reminder/route.ts
+++ src/app/api/reminder/route.ts
@@ -1,14 +1,12 @@
 import { monthlyReminder } from "@/app/services/server/remindersService";
 
 export async function GET(req: Request) {
-  // const url = new URL(req.url);
-  // const secret = url.searchParams.get("secret");
-
-  // if (secret !== process.env.CRON_SECRET) {
-  //   return new Response(JSON.stringify({ error: "Unauthorized" }), {
-  //     status: 401,
-  //   });
-  // }
+  const authHeader = req.headers.get('authorization');
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized', {
+      status: 401,
+    });
+  }
   monthlyReminder();
   console.log("Monthly reminders triggered");
   return new Response("Monthly reminders triggered");
