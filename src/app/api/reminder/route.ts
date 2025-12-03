7b743234667abe71c90c81417b1007b647d56cec
שרה תמר אמיתי <saratama@g.jct.ac.il>
2025-12-03 09:46:40 +0200

    10:00 no auth

--- src/app/api/reminder/route.ts
+++ src/app/api/reminder/route.ts
@@ -1,12 +1,12 @@
 import { monthlyReminder } from "@/app/services/server/remindersService";
 
 export async function GET(req: Request) {
-  const authHeader = req.headers.get('authorization');
-  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    return new Response('Unauthorized', {
-      status: 401,
-    });
-  }
+  // const authHeader = req.headers.get('authorization');
+  // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+  //   return new Response('Unauthorized', {
+  //     status: 401,
+  //   });
+  // }
   monthlyReminder();
   console.log("Monthly reminders triggered");
   return new Response("Monthly reminders triggered");
