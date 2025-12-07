796d8616960223f6f0f1acb4243c54b45289e541
שרה תמר אמיתי <saratama@g.jct.ac.il>
2025-12-07 09:41:29 +0200

    Revert to 189d907b4157df1287475935e657430d40af6ca6 state

--- src/app/components/AdvancedExpense/AdvancedExpense.tsx
+++ src/app/components/AdvancedExpense/AdvancedExpense.tsx
@@ -13,7 +13,7 @@ import {
 } from "@mui/material";
 import type { Member,SplitDetail } from "@/app/types/types";
 import { useAdvancedExpense } from "@/app/hooks/useAdvancedExpense";
-import { toMoney, formatILS } from "@/app/utils/money";
+import {  formatILS } from "@/app/utils/money";
 import styles from "./AdvancedExpense.module.css";
 import { useState } from "react";
 
@@ -128,7 +128,7 @@ export default function AdvancedExpense({
               size="small"
               fullWidth
               value={amountValue}
-              onChange={(e) => setAmountValue(toMoney(e.target.value))}
+              onChange={(e) => setAmountValue(Number(e.target.value) || 0)}
               className={styles.metaInput}
             />
           </div>
